b6dd058a06f2291b30382676f4aacf0a098c8fa3
Matías Pierobon <matias.pierobonm@gmail.com>
2018-11-24 21:38:11 -0300

    seeds!

--- src/seeds.js
+++ src/seeds.js
@@ -1,28 +1,16 @@
 const mongoose = require('mongoose');
 const faker = require('faker');
+const users = require('./controllers/users');
 
-const products = require('./controllers/products');
-const types = require('./controllers/types');
-
-const Product = (name, salePrice, costPrice, type) =>
-  products.create({ name, salePrice, costPrice, type });
-const Type = (initials, description) => types.create({ initials, description });
+const User = (username, email, password, avatar) => users.create({ username, email, password, avatar });
 
 mongoose.connect(process.env.DB_URL).then(async () => {
   for (let index = 0; index < 5; index++) {
     const description = faker.commerce.department();
-    const type = await Type(
-      description.toUpperCase().substring(0, 2),
-      description
-    );
+    const username = faker.internet.userName();
+    const email = faker.internet.email(username);
+    const avatar = faker.internet.avatar();
+    const user = User(username, email, "12345", avatar)
     console.log(type);
-
-    for (let index = 0; index < 20; index++) {
-      const name = faker.commerce.productName();
-      const costPrice = faker.commerce.price();
-      const salePrice = costPrice * (index / 20 + 1);
-      const product = await Product(name, costPrice, salePrice, type._id);
-      console.log(product);
-    }
   }
 });
