1d125b13eca8b7021a23a3f654afe9791a198bfe
Matías Pierobon <matias.pierobonm@gmail.com>
2018-11-24 21:38:01 -0300

    add endpoints for jwt

--- src/api.js
+++ src/api.js
@@ -1,20 +1,32 @@
 const status = require('http-status');
 const users = require('./controllers/users');
+const login = require('./controllers/login');
 const userValidator = require('./validators/users');
 
 const handle = fn => (req, res, next) => fn(req, res, next).catch(next);
+
+const regex = /(\S+)\s+(\S+)/
 
 module.exports = app => {
   const resource = require('./utils/resource')(app);
 
   app.get('/valid', handle(async (req, res) => {
-    const { id } = req.params;
-    res.status(status.OK).json(await users.valid(id));
+    const { authorization } = req.headers;
+    if(!authorization){
+      throw error(401, "No authorization header provied")
+    }
+
+    const token = authorization.match(regex)[2]
+    if(!token){
+      throw error(401, "No jwt provied")
+    }
+
+    res.status(status.OK).json(await login.valid(token));
   }));
 
   app.post('/login', handle(async (req, res) => {
-      const { id } = req.params;
-      res.status(status.OK).json(await users.buy(id));
+      const { email, password } = req.body;
+      res.status(status.OK).json(await login.signIn(email, password));
   }));
 
   resource('/', users, userValidator);
