c2411c0d69b5e4fb9f7f638bdc7a2e4d52b9ee51
Matías Pierobon <matias.pierobonm@gmail.com>
2018-11-29 02:46:55 -0300

    mod seeds

--- src/seeds.js
+++ src/seeds.js
@@ -2,16 +2,23 @@ const mongoose = require('mongoose');
 const faker = require('faker');
 const users = require('./controllers/users');
 
-const User = (username, email, password, avatar) => users.create({ username, email, password, avatar });
+const User = (username, email, password, avatar, _id ) => users.create({ _id, username, email, password, avatar });
 
 console.log(process.env.DB_URL);
 
 mongoose.connect(process.env.DB_URL).then(async () => {
-  for (let index = 0; index < 5; index++) {
-    const username = faker.internet.userName();
-    const email = faker.internet.email(username);
-    const avatar = faker.internet.avatar();
-    const user = await User(username, email, "12345", avatar)
+    let username = "Employee";
+    let avatar = faker.internet.avatar();
+    let email = "employee@ksupermarket.com";
+    let id = "5bfaf2408587d4003a2b0ded";
+    let user = await User(username, email, "employee", avatar, id)
     console.log(user);
-  }
+
+    username = "user";
+    avatar = faker.internet.avatar();
+    email = "user@ksupermarket.com";
+    user = await User(username, email, "user", avatar)
+    console.log(user);
+
+    process.exit();
 });
